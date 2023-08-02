fabbfd69dcd2699e59c70ac81819c80fe84e915b
Emanoel Silva <emanoelsilva.jua@gmail.com>
2023-08-02 17:25:21 -0300

    demais comando cypress

--- cypress/e2e/CAT-TAT.cy.js
+++ cypress/e2e/CAT-TAT.cy.js
@@ -3,13 +3,15 @@
 // variavel encadeada
 describe('Central de Atendimento ao Cliente TAT', () => {
 
+    const THREE_SECONDS_IN_MS = 3000
+
   beforeEach(() => {
       cy.visit('./src/index.html')
-      //cy.visit('')
-      
+     
+      cy.viewport(550, 850)//Tamanho da tela
   });
 
-  it('verificar o título da aplicação', () => {
+it('verificar o título da aplicação', () => {
       
       
       //verificação se o titulo esta correto
@@ -21,7 +23,9 @@ describe('Central de Atendimento ao Cliente TAT', () => {
   it('Preencher os campos obrigatórios e enviar o formulário', () => {
       //Mapear cada campo obrigatório
       const longText = 'teste,teste,teste,teste,teste,teste,teste,teste,teste,teste,teste,teste,teste,teste,teste,teste,teste,teste,teste,teste,teste,teste,teste,teste,teste,teste,teste,teste,teste,teste,teste,teste,'
-    
+
+
+      cy.clock() // Para congelar o relogio do navegador
       //Nome
       cy.get('#firstName').type('Emanoel')
           .should('be.visible')
@@ -38,18 +42,25 @@ describe('Central de Atendimento ao Cliente TAT', () => {
       cy.contains('button', 'Enviar').click()
 
       cy.get('.success').should('be.visible')// mostra que o teste esta passando
+
+      cy.tick(THREE_SECONDS_IN_MS)
       
+      cy.get('.success').should('not.be.visible')
   });
   it('exibe mensagem de erro ao submeter formulário com um email com formatação invalida', () => {
+    cy.clock()
 
-      cy.get('#firstName').type('Emanoel')
-      cy.get('#lastName').type('Silva') 
-      cy.get('#email').type('teste@teste,com') // tentar com o email inválido
-      cy.get('#open-text-area').type('teste')
-      cy.get('.button[type="submit"]').click()
+    cy.get('#firstName').type('Emanoel')
+    cy.get('#lastName').type('Silva') 
+    cy.get('#email').type('teste@teste,com') // tentar com o email inválido
+    cy.get('#open-text-area').type('teste')
+    cy.get('.button[type="submit"]').click()
 
-      cy.get('.error').should('be.visible') // a mensagem de error deve está visível
+    cy.get('.error').should('be.visible') // a mensagem de error deve está visível
+    cy.tick(THREE_SECONDS_IN_MS)
+      cy.get('.error').should('not.be.visible') // visualizar se a mensagem de error desapareceu
       
+
   });
 
   it('campo de telefone continua vazio quando preenchido com o valor não-numerico', () => {
@@ -59,7 +70,7 @@ describe('Central de Atendimento ao Cliente TAT', () => {
   });
 
   it('exibe mensagem de erro quando o telefone se torna obrigatório mas é preenchido antes do envio do formulário', () => {
-
+    cy.clock()
       cy.get('#firstName').type('Emanoel')
       cy.get('#lastName').type('Silva') 
       cy.get('#email').type('teste@teste.com') 
@@ -68,7 +79,9 @@ describe('Central de Atendimento ao Cliente TAT', () => {
       cy.get('.button[type="submit"]').click()
       // vendo se a mesnagem de error esta visivel
       cy.get('.error').should('be.visible') // 
-      
+    cy.tick(THREE_SECONDS_IN_MS)
+      cy.get('.error').should('not.be.visible') // visualizar se a mensagem desapareceu
+
       
   });
 
@@ -108,11 +121,19 @@ describe('Central de Atendimento ao Cliente TAT', () => {
       cy.get('.error')
           .should('be.visible')
       
-  });
+  }); 
   it('enviar o formulário com sucesso usando um comando customizado', () => {
+
+    cy.clock()
 
       cy.fillMandatoryFieldsAndSubmit()        
       cy.get('.success').should('be.visible')
+
+      cy.tick(THREE_SECONDS_IN_MS)
+
+    cy.get('.success').should('not.be.visible')
+
+
   });
 
   it('Selecione um produto (YouTube) por seu texto', () => {
@@ -210,11 +231,54 @@ describe('Central de Atendimento ao Cliente TAT', () => {
       
   })
 
+
+  it('Exibe e esconde as mensagens de sucesso e erroo usando o invoke', () => {
+    cy.get('.success') // quando for sucesso
+      .should('not.be.visible')
+      .invoke('show') // mostra a mensagem
+      .should('be.visible')
+      .and('contain', 'Mensagem enviada com sucesso.') //verifica a mensagem e o "and" executa junto o comando anterior
+      .invoke('hide') // esconde a mensagem
+      .should('not.be.visible')
+
+    cy.get('.error') // quando for o erro
+      .should('not.be.visible') //verifica se esta visivel
+      .invoke('show') // mostra a,longText mensagem
+      .should('be.visible') //valida se esta a amostra a mensagem
+      .and('contain', 'Valide os campos obrigatórios!')
+      .invoke('hide')// esconde a mensagem
+      .should('not.be.visible')//valida de a mensagem saiu
+    
+  });
+
+  it('preenche a area de texto usando o comando invoke', () => {
+
+   const longText = Cypress._.repeat('alysson',10)
+    cy.get('#open-text-area')
+    .invoke('val', longText) //invoca o valor na area do texto - "val" é uma função do jQuery
+    .should('have.value',longText) // deve ter o valor que esta na variavel
+
+    
+  });
+
+  it('Faz uma requisição HTTP', () => {
+    //cy.request('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html')
+    cy.request('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html')
+    .should((Response) => {
+
+        //console.log(Response) comando para verificar a resposta
+        //Função de callback
+        const { status, statusText, body } = Response
+        expect(status).to.equal(200)
+        expect(statusText).to.equal('OK')
+        expect(body).to.include('CAC TAT')
+
+    })
+    
+  });
+  
   
 
-
-
-  
 })
 
 
--- cypress/e2e/privacy.cy.js
+++ cypress/e2e/privacy.cy.js
@@ -1,8 +1,10 @@
 
 it.only('testa a página da política de privacidade de forma independente', () => {
     
+        Cypress._.times(3, () => { //Para repetir os teste, neste caso 3 vezes
+
         cy.visit('./src/privacy.html')
-        cy.contains('Talking About Testing').should('be.visible') //verificando se o texto em questão é visiveal
+        cy.contains('Talking About Testing').should('be.visible') //verificando se o texto em questão é visivel
   
-    
+        })
 })
